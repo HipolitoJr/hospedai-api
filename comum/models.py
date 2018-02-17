7cf0f7a788f797d46b1f0f3d9d86a9f719029bc5
HipolitoJr <hipolitocavalcantejr@gmail.com>
2018-02-16 22:09:33 -0300

    Criação modelos da API

--- comum/models.py
+++ comum/models.py
@@ -1,3 +1,61 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Endereco(models.Model):
+
+    logradouro = models.CharField('Logradouro', max_length=255, blank=False, null=False)
+    numero = models.IntegerField('Numero', blank=False, null=False)
+    cep = models.IntegerField('CEP', blank=True, null=False)
+    bairro = models.CharField('Bairro', max_length=255, blank=True, null=False)
+    cidade = models.CharField('Cidade', max_length=255, blank=True, null=False)
+    estado = models.CharField('Estado', max_length=255, blank=True, null=False)
+
+    def __str__(self):
+        return "%s, Nº - %s, %s" %(self.logradouro, self.numero, self.cidade, self.estado)
+
+
+class Hotel(models.Model):
+
+    razao_social = models.CharField('Razao social', max_length=255, blank=False, null=False)
+    telefone = models.IntegerField('Telefone', blank=False, null=False)
+    valor_diaria = models.FloatField('Valor diaria', blank=False, null=False)
+
+    endereco = models.OneToOneField('Endereco', on_delete=models.SET_NULL, blank=True, null=True)
+    usuario = models.OneToOneField(User, related_name='hotel')
+
+    def __str__(self):
+        return self.razao_social
+
+
+class Hospede(models.Model):
+
+    nome = models.CharField('Nome', max_length=255, blank=False, null=False)
+    telefone = models.IntegerField('Telefone', blank=False, null=False)
+    email = models.CharField('Email', max_length=255, blank=False, null=False)
+    cpf = models.IntegerField('CPF', blank=False, null=False)
+
+    endereco = models.OneToOneField('Endereco', on_delete=models.SET_NULL, blank=True, null=True)
+    hotel = models.ForeignKey('Hotel', related_name='clientes_hotel', on_delete=models.CASCADE, blank=False, null=False)
+
+    def __str__(self):
+        return self.nome
+
+
+class Hospedagem(models.Model):
+
+    TIPO_STATUS = (
+        ('aberta', 'Aberta'),
+        ('fechada', 'Fechada'),
+    )
+
+    data_checkin = models.DateTimeField('Data checkin', auto_now_add=True, blank=False, null=False)
+    data_checkout = models.DateTimeField('Data checkout', auto_now=True, blank=True, null=True)
+    status = models.CharField('Status', max_length=255, choices=TIPO_STATUS, blank=False, null=False)
+
+    hospede = models.OneToOneField('Hospede', on_delete=models.SET_NULL, blank=True, null=True)
+    hotel = models.OneToOneField('Hotel', on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return "%s - %s (%s)" %(self.hospede, self.status, self.hotel)
--- hospedai/settings.py
+++ hospedai/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #Apps DRF
+    'rest_framework',
+    'rest_framework.authtoken',
+
+    #Apps Internas
+    'comum',
+
 ]
 
 MIDDLEWARE = [
@@ -74,10 +82,20 @@ WSGI_APPLICATION = 'hospedai.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hospedai',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
+
 }
 
 
